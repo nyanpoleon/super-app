52d0c56c39b5e7656da4c527c006a7ddcaad028b
Purujit Pradhan <purujit26@gmail.com>
2023-04-05 21:12:22 +0530

    yaay register.jsx

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,22 +1,10 @@
-// import register from './register.jsx'
+import Register from './Register.jsx'
 import './App.css';
 // import landingimage from './resources/landingimage.png';
 
 function App() {
   return (
-    <div className='register-page'>
-      <div className='left-half'>
-        <div className='imgheader'>
-          <div>Already have an account?</div> 
-          <button className='loginBtn'>Login</button>
-        </div>
-        <div className='imgfooter'>
-          Discover new things on Superapp
-        </div>
-      </div>
-      <div className='right-half'>the form will go here</div>
-      
-    </div>
+    <Register />
     
     
     
