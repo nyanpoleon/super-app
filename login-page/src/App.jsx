0d24f923a8fac6a691ac8da1904a90e19b364eda
Purujit Pradhan <purujit26@gmail.com>
2023-04-05 21:07:07 +0530

    so far working

---
+++ login-page/src/App.jsx
@@ -0,0 +1,26 @@
+// import register from './register.jsx'
+import './App.css';
+// import landingimage from './resources/landingimage.png';
+
+function App() {
+  return (
+    <div className='register-page'>
+      <div className='left-half'>
+        <div className='imgheader'>
+          <div>Already have an account?</div> 
+          <button className='loginBtn'>Login</button>
+        </div>
+        <div className='imgfooter'>
+          Discover new things on Superapp
+        </div>
+      </div>
+      <div className='right-half'>the form will go here</div>
+      
+    </div>
+    
+    
+    
+  );
+}
+
+export default App;
