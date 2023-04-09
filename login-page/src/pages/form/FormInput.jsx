ac0ed1ca606a6d22947e6d1f7878da2360299f33
Purujit Pradhan <purujit26@gmail.com>
2023-04-10 00:13:30 +0530

    added a lot of css, along with a logo

--- login-page/src/pages/form/FormInput.jsx
+++ login-page/src/pages/form/FormInput.jsx
@@ -1,10 +1,11 @@
 import "../Register.css"
 
 const FormInput = (props) => {
+    const {label, id, onChange, ...inputProps} = props;
     return(
         <div className="formInput">
             {/* <label>username</label> */}
-            <input placeholder={props.placeholder} onChange={event=>props.setFormData(event.target.value)}/>
+            <input {...inputProps} onChange={onChange}/>
         </div>
     )
 }
