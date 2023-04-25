812656f920554955a73a67bd1a825e3dc61f0c9f
Purujit Pradhan <purujit26@gmail.com>
2023-04-25 21:24:23 +0530

    creating cards page

--- login-page/src/pages/Category.jsx
+++ login-page/src/pages/Category.jsx
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Category.css"
+import Logo from "../resources/Super app.png"
 
 function Category() {
+
+    const [cards] = useState([
+        
+    ])
     return(
         <div className="main">
-            <div className="leftpanel">left panel</div>
+            <div className="leftpanel">
+                <div className="logo"><img className="logo" src={Logo} alt="" /></div>
+                <div className="bigtext">Choose your entertainment category</div>
+            </div>
             <div className="rightpanel">cards go here</div>
         </div>
         
